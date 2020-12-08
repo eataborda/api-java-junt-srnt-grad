5c2436650ba842e7bc17d53aac2ba93ad9502cfa
Edwin Taborda <eataborda.m@gmail.com>
2020-12-08 10:33:31 -0500

    Add basic structure test

---
+++ src/test/java/BasicAPIValidation.java
@@ -0,0 +1,19 @@
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+@RunWith(SerenityRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class BasicAPIValidation {
+    @BeforeClass
+    public static void setup(){
+
+    }
+    @Test
+    public void statusCodeValidation(){
+
+    }
+}
